32877b5573b8ff526b7dda22b440b9b872dc99b2
Derrick Ampire <ampire90@gmail.com>
2022-10-04 22:05:37 +0300

    shell script to apply yamls

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Apply config and secret
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+#Apply deployments
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f reverseproxy-deployment.yaml
+
+#Apply services
+kubectl apply -f backend-feed-service.yaml
+kubectl apply -f backend-user-service.yaml
+kubectl apply -f frontend-service.yaml
+kubectl apply -f reverseproxy-service.yaml
